90b9b29bcbdfd72c4d3eb848dc04bea0cefba60b
Edimaique <edimaiqueacacio@gmail.com>
2025-04-26 21:23:56 -0300

    mudança no arquivo procfile

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
+#Armazenar as configurações do ambiente de desenvolvimento
+from os import environ # acessa as variáveis de ambiente
+
+from dotenv import load_dotenv #carregamento das variáveis de ambiente nesse arquivo
+
+load_dotenv()
+
 class Config():
-    SQLALCHEMY_DATABASE_URI = environ.get("ULR_DATABASE_DEV") 
-    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith('mysql:'):
-        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace('mysql:', 'mysql+pymysql:')
-    SQLALCHEMY_TRACK_MODIFICATIONS=False
+    SQLALCHEMY_DATABASE_URI = environ.get("ULR_DATABASE_DEV") # puxa a variável e utiliza para a conexão
+    SQLALCHEMY_TRACK_MODIFICATIONS=False #Otimiza as queries no banco de dados
+    
